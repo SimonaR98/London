e1e0f69e8596ad9c18b93fef862c66407085b60b
SimonaR98 <simona.russo.aa98@gmail.com>
2026-02-09 23:01:07 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -47,3 +47,23 @@ function updateCountdown() {
 
 setInterval(updateCountdown, 1000);
 updateCountdown();
+
+//Freccia per tornare su
+const backToTop = document.getElementById("backToTop");
+
+window.addEventListener("scroll", () => {
+    // Appare dopo 400px di scroll
+    if (window.scrollY > 400) {
+        backToTop.classList.add("show");
+    } else {
+        backToTop.classList.remove("show");
+    }
+});
+
+backToTop.addEventListener("click", (e) => {
+    e.preventDefault();
+    window.scrollTo({
+        top: 0,
+        behavior: "smooth"
+    });
+});
